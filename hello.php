eb8bd9e37cb878528513da50c568c0bfdea7b9e0
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-04-26 09:45:18 +0300

    Practicing variables, prints, etc

---
+++ hello.php
@@ -0,0 +1,48 @@
+<?php
+
+// $message = "\nBye now";
+// $myinteger = 4;
+// $myfloat = 3.14;
+// $aboolean = true;
+// $anotherboolean = false;
+// $mynull = null;
+// var_dump($mynull, $anotherboolean);
+// var_dump($aboolean);
+// var_dump($anotherboolean);
+// var_dump($myfloat);
+// var_dump($myinteger);
+// echo "Hello world, my name is Anigo Mont\royas.  You ran around the track.\n";
+// echo $message;
+// var_dump($myfloat * $myinteger);
+// echo $message . " " . $message;
+// $kid = "111.1";
+// $num = 3;
+// var_dump($kid * $num);
+//
+// $articles = array("First post", "Second post", "Last post");
+// var_dump($articles);
+// echo $articles[1];
+$alice = array(
+  "name" => "Alice",
+  "age" => 42,
+  "profession" => "writer"
+);
+
+$john = array(
+  "name" => "John",
+  "age" => 37,
+  "profession" => "programmer"
+);
+
+$slagathorpe = array(
+  "name" => "Slagathorpe",
+  "age" => 56,
+  "profession" => "Ghostbuster"
+);
+
+$people = array($alice, $john, $slagathorpe);
+var_dump($people);
+
+foreach($people as $person){
+  echo $person["name"], " ", $person["age"], " ", $person["profession"];
+}
