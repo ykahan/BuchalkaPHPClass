197b186c4c6dcc16731e7cb5c5c925af87edfc7f
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-04-28 17:08:54 +0300

    foreach loop in an HTML li

--- Code/phpWithHtml.php
+++ Code/phpWithHtml.php
@@ -7,8 +7,15 @@
   <link rel="stylesheet" type="text/css" title="Cool stylesheet" href="style.css">
 </head>
 <body>
+  <?php $names = ["Yehoshua", "Kahan", "Abba", "Me"]; ?>
   <div id="root"></div>
   <p>Here's a paragraph <?php echo "that contains some PHP"; ?></p>
+  <p>Here's a list of names</p>
+  <ul>
+    <?php foreach($names as $name){
+      echo "<li>Current name: $name</li>";
+    }?>
+  </ul>
   <script src="main.js" type="text/javascript"></script>
 </body>
 </html>
