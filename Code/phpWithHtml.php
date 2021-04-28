446a778bf2cccaec6509f9876dd4691fb3ec0c46
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-04-28 16:55:12 +0300

    put php in the html

--- Code/phpWithHtml.php
+++ Code/phpWithHtml.php
@@ -1,18 +1,14 @@
-<?php
-
-echo "<!DOCTYPE html>";
-echo "<html lang=\"en\">";
-echo "<head>";
-echo "<meta charset=\"UTF-8\">";
-echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
-echo "<title>PHP With HTML</title>";
-echo "<link rel=\"stylesheet\" type=\"text/css\" title=\"Cool stylesheet\" href=\"style.css\">";
-echo "</head>";
-echo "<body>";
-echo "<h1>We're using HTML with PHP</h1>";
-echo "<p>Isn't that cool?</p>";
-echo "<p>Here's a line with a \nline break in it.</p>";
-echo "<div id=\"root\"></div>";
-echo "<script src=\"main.js\" type=\"text/javascript\"></script>";
-echo "</body>";
-echo "</html>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP With HTML</title>
+  <link rel="stylesheet" type="text/css" title="Cool stylesheet" href="style.css">
+</head>
+<body>
+  <div id="root"></div>
+  <p>Here's a paragraph <?php echo "that contains some PHP"; ?></p>
+  <script src="main.js" type="text/javascript"></script>
+</body>
+</html>
