642ba160c8a87b3459f327a3c7eb11b92cd3605c
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-04-28 16:16:37 +0300

    Using PHP with HTML

---
+++ Code/phpWithHtml.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "<!DOCTYPE html>";
+echo "<html lang=\"en\">";
+echo "<head>";
+echo "<meta charset=\"UTF-8\">";
+echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
+echo "<title>PHP With HTML</title>";
+echo "<link rel=\"stylesheet\" type=\"text/css\" title=\"Cool stylesheet\" href=\"style.css\">";
+echo "</head>";
+echo "<body>";
+echo "<h1>We're using HTML with PHP</h1>";
+echo "<p>Isn't that cool?</p>";
+echo "<div id=\"root\"></div>";
+echo "<script src=\"main.js\" type=\"text/javascript\"></script>";
+echo "</body>";
+echo "</html>";
