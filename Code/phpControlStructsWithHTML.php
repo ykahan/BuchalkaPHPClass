daae577c10285dd2ef9b0aa36bc7b61ce7e4ecc8
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-05-05 12:54:56 +0300

    reduced php/html interaction by putting each if, elseif, or else into its own set of php braces.

--- Code/phpControlStructsWithHTML.php
+++ Code/phpControlStructsWithHTML.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <?php
+  $hour = 12;
+  ?>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Boilerplate</title>
@@ -8,18 +11,16 @@
 </head>
 <body>
   <div id="root"></div>
-  <?php
-  $hour = 12;
-  if($hour < 12){
-    echo "G'mornin!";
-  } elseif($hour < 18){
-    echo "G'afternoon!";
-  } elseif($hour < 22){
-    echo "G'evenin'!";
-  } else {
-    echo "G'night!";
-  }
-  ?>
+  <?php if($hour < 12): ?>
+    G'mornin!
+  <?php elseif($hour < 18) : ?>
+    G'afternoon!  Hiya!
+  <?php elseif($hour < 22): ?>
+    G'evenin'!
+  <?php else: ?>
+  G'night!
+<?php endif; ?>
+
   <script src="main.js" type="text/javascript"></script>
-  </body>
-  </html>
+</body>
+</html>
