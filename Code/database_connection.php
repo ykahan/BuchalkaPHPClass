3d59dd0050766bcf400c97a93f6636985094b651
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-05-09 17:57:06 +0300

    Connected to database

--- Code/database_connection.php
+++ Code/database_connection.php
@@ -2,5 +2,14 @@
 
 $db_host = "localhost";
 $db_name = "cms_buchalka_course";
-$table = "articles";
-$password = "abcd";
+$db_user = "cms_www";
+$db_password = "abcd";
+
+$conn =  mysqli_connect($db_host, $db_user, $db_password, $db_name);
+
+if(mysqli_connect_error()){
+	echo mysqli_connect_error();
+	// exit;
+} else {
+	echo "Connected";
+}
