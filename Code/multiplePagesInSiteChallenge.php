f40726ff5b80b4b52c767f50290ce8c8fbcb9328
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-05-10 16:03:12 +0300

    End of section challenge

---
+++ Code/multiplePagesInSiteChallenge.php
@@ -0,0 +1,30 @@
+<?php
+
+$posts = [1 => 'Good news', 3 => 'Read this', 5 => 'Important announcement'];
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Posts</title>
+    </head>
+    <body>
+
+    <h1>Posts</h1>
+
+    <ul>
+        <?php foreach($posts as $id => $title): ?>
+
+            <li>
+                <a href="post.php?id=<?= $id ?>">
+                    <?=
+                    $posts[$id]
+                    ?>
+                </a>
+            </li>
+
+        <?php endforeach; ?>
+    </ul>
+
+    </body>
+</html>
