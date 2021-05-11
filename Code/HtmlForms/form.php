c45756544c373a13358d21a0a50d54d1c881ddda
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-05-11 10:52:25 +0300

    Gave form tag a method attribute of post, causing it to send data without putting it into the URL.  Important security for passwords and suchlike

--- Code/HtmlForms/form.php
+++ Code/HtmlForms/form.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>HTML Forms</title>
-  <link rel="stylesheet" type="text/css" title="Cool stylesheet" href="style.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>HTML Forms</title>
+	<link rel="stylesheet" type="text/css" title="Cool stylesheet" href="style.css">
 </head>
 <body>
-	<form action="process_form.php">
-		<input name="search">
+	<form method="post">
+		<input name="username">
+		<input name="password" type="password">
 		<button>Send</button>
 	</form>
-  <div id="root"></div>
-  <script src="main.js" type="text/javascript"></script>
+	<div id="root"></div>
+	<script src="main.js" type="text/javascript"></script>
 </body>
 </html>
