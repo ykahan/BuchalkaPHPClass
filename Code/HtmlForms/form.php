97b28e19fac852ff535a5c472bbb62b25a540222
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-05-11 11:27:50 +0300

    Added a variety of different input types

--- Code/HtmlForms/form.php
+++ Code/HtmlForms/form.php
@@ -16,6 +16,20 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 </head>
 <body>
 <form method="post">
+	<div>text:<input type="text"></div>
+	<div>password:<input type="password"></div>
+	<div>tel:<input type="tel"></div>
+	<div>url:<input type="url"></div>
+	<div>date:<input type="date"></div>
+	<div>time:<input type="time"></div>
+	<div>color:<input type="color"></div>
+	<div>email:<input type="email"></div>
+	<div>month:<input type="month"></div>
+	<div>range:<input type="range"></div>
+	<div>hidden:<input type="hidden"></div>
+	<div>number:<input type="number"></div>
+	<div>search:<input type="search"></div>
+	<div>datetime-local:<input type="datetime-local"></div>
 <input name="username">
 <input name="password" type="password">
 <button>Send</button>
