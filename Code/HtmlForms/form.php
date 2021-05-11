d4fa84d5ea98b2255b1afbae73f14aaa3677b8c5
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-05-11 11:00:22 +0300

    Added checking code to only display var_dump if post was submitted

--- Code/HtmlForms/form.php
+++ Code/HtmlForms/form.php
@@ -1,24 +1,26 @@
 <?php
 
-var_dump($_POST);
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+	var_dump($_POST);
+}
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>HTML Forms</title>
-	<link rel="stylesheet" type="text/css" title="Cool stylesheet" href="style.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>HTML Forms</title>
+<link rel="stylesheet" type="text/css" title="Cool stylesheet" href="style.css">
 </head>
 <body>
-	<form method="post">
-		<input name="username">
-		<input name="password" type="password">
-		<button>Send</button>
-	</form>
-	<div id="root"></div>
-	<script src="main.js" type="text/javascript"></script>
+<form method="post">
+<input name="username">
+<input name="password" type="password">
+<button>Send</button>
+</form>
+<div id="root"></div>
+<script src="main.js" type="text/javascript"></script>
 </body>
 </html>
