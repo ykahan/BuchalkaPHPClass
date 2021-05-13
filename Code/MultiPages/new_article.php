567174f4626e7abee81aba5126ea6a3345ad3703
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-05-13 14:32:49 +0300

    Created code to demonstrate use of Prepared Statements to avert SQL injection attacks

--- Code/MultiPages/new_article.php
+++ Code/MultiPages/new_article.php
@@ -6,21 +6,41 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 	require("includes/database.php");
 
-	$sql_query = "INSERT INTO articles (title, content, published_at)
 
-	-- the mysqli_escape_string function automatically escapes any quotation
-	-- marks in the values passed in, thus defeating SQL injection attacks
-	VALUES ('" . mysqli_escape_string($conn, $_POST['title']) . "',
-		'". mysqli_escape_string($conn, $_POST['content']) .  "',
-		'". mysqli_escape_string($conn, $_POST['published_at']) ."')";
+		// -- the mysqli_escape_string function automatically escapes any quotation
+		// -- marks in the values passed in, thus defeating SQL injection attacks
+	// $sql_query = "INSERT INTO articles (title, content, published_at)
+	// VALUES ('" . mysqli_escape_string($conn, $_POST['title']) . "',
+	// 	'". mysqli_escape_string($conn, $_POST['content']) .  "',
+	// 	'". mysqli_escape_string($conn, $_POST['published_at']) ."')";
 
-		$results = mysqli_query($conn, $sql_query);
 
-		if($results === false){
+// $results = mysqli_query($conn, $sql_query);
+
+	// using a Prepared Statement to foil SQL injection attacks.
+	$sql_query = "INSERT INTO articles(title, content, published_at)
+                   VALUES(?, ?, ?)";
+
+  $prep_stmt = mysqli_prepare($conn, $sql_query);
+
+		// if($results === false){
+		if($prep_stmt === false){
 			echo mysqli_error($conn);
 		} else {
+			// binding params to create prepared statement
+			$title = $_POST['title'];
+			$content = $_POST['content'];
+			$published_at = $_POST['published_at'];
+			mysqli_stmt_bind_param($prep_stmt, "sss", $title, $content, $published_at);
+
+			// executing prepared statement
+		 if(mysqli_stmt_execute($prep_stmt)){
+
 			$id = mysqli_insert_id($conn);
 			echo "Inserted record with ID" . $id;
+		} else{
+			echo mysqli_stmt_error($prep_stmt);
+		}
 		}
 	}
 
