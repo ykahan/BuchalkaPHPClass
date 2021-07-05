2eef55a8ddc2af168cd01b48a509e32cb1141763
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-07-05 11:40:09 +0300

    View $_POST variable

--- Code/HtmlForms/input_types.php
+++ Code/HtmlForms/input_types.php
@@ -1,32 +1,39 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+	var_dump($_POST);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Input Types</title>
-  <link rel="stylesheet" type="text/css" title="Cool stylesheet" href="style.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Input Types</title>
+	<link rel="stylesheet" type="text/css" title="Cool stylesheet" href="style.css">
 </head>
 <body>
-  <div id="root">
-	<form method="post">
-		<div>text: <input type="text"></div>
-		<div>password: <input type="password"></div>
-		<div>telephone: <input type="tel"></div>
-		<div>URL: <input type="url"></div>
-		<div>date: <input type="date"></div>
-		<div>time: <input type="time"></div>
-		<div>week: <input type="week"></div>
-		<div>color: <input type="color"></div>
-		<div>email: <input type="email"></div>
-		<div>month: <input type="month"></div>
-		<div>range: <input type="range"></div>
-		<div>hidden: <input type="hidden"></div>
-		<div>number: <input type="number"></div>
-		<div>search: <input type="search"></div>
-		<div>datetime-local: <input type="datetime-local"></div>
+	<div id="root">
+		<form method="post">
+			<div>text: <input type="text"></div>
+			<div>password: <input type="password"></div>
+			<div>telephone: <input type="tel"></div>
+			<div>URL: <input type="url"></div>
+			<div>date: <input type="date"></div>
+			<div>time: <input type="time"></div>
+			<div>week: <input type="week"></div>
+			<div>color: <input type="color"></div>
+			<div>email: <input type="email"></div>
+			<div>month: <input type="month"></div>
+			<div>range: <input type="range"></div>
+			<div>hidden: <input type="hidden"></div>
+			<div>number: <input type="number"></div>
+			<div>search: <input type="search"></div>
+			<div>datetime-local: <input type="datetime-local"></div>
 
-	</form>
-</div>
-  <script src="main.js" type="text/javascript"></script>
+		</form>
+	</div>
+	<script src="main.js" type="text/javascript"></script>
 </body>
 </html>
