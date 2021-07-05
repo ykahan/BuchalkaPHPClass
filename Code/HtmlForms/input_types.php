69921fef2882fcf9abe677b530474cfb4b3f9034
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-07-05 12:06:38 +0300

    submit button and name attributes

--- Code/HtmlForms/input_types.php
+++ Code/HtmlForms/input_types.php
@@ -16,21 +16,22 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <body>
 	<div id="root">
 		<form method="post">
-			<div>text: <input type="text"></div>
-			<div>password: <input type="password"></div>
+			<div>text: <input type="text" name="text"></div>
+			<div>password: <input type="password" name="password"></div>
 			<div>telephone: <input type="tel"></div>
-			<div>URL: <input type="url"></div>
-			<div>date: <input type="date"></div>
-			<div>time: <input type="time"></div>
-			<div>week: <input type="week"></div>
-			<div>color: <input type="color"></div>
-			<div>email: <input type="email"></div>
-			<div>month: <input type="month"></div>
-			<div>range: <input type="range"></div>
-			<div>hidden: <input type="hidden"></div>
-			<div>number: <input type="number"></div>
-			<div>search: <input type="search"></div>
-			<div>datetime-local: <input type="datetime-local"></div>
+			<div>URL: <input type="url" name="url"></div>
+			<div>date: <input type="date" name="date"></div>
+			<div>time: <input type="time" name="time"></div>
+			<div>week: <input type="week" name="week"></div>
+			<div>color: <input type="color" name="color"></div>
+			<div>email: <input type="email" name="email"></div>
+			<div>month: <input type="month" name="month"></div>
+			<div>range: <input type="range" name="range"></div>
+			<div>hidden: <input type="hidden" name="hidden"></div>
+			<div>number: <input type="number" name="number"></div>
+			<div>search: <input type="search" name="search"></div>
+			<div>datetime-local: <input type="datetime-local" name="datetime-local"></div>
+			<button>Submit</button>
 
 		</form>
 		<!-- testing token -->
