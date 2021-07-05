b001f81e2f34f0821dcff699ba5de5050480619e
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-07-05 12:35:10 +0300

    value attributes filled

--- Code/HtmlForms/input_types.php
+++ Code/HtmlForms/input_types.php
@@ -16,21 +16,24 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <body>
 	<div id="root">
 		<form method="post">
-			<div>text: <input type="text" name="text"></div>
-			<div>password: <input type="password" name="password"></div>
-			<div>telephone: <input type="tel"></div>
-			<div>URL: <input type="url" name="url"></div>
-			<div>date: <input type="date" name="date"></div>
-			<div>time: <input type="time" name="time"></div>
-			<div>week: <input type="week" name="week"></div>
-			<div>color: <input type="color" name="color"></div>
-			<div>email: <input type="email" name="email"></div>
-			<div>month: <input type="month" name="month"></div>
-			<div>range: <input type="range" name="range"></div>
-			<div>hidden: <input type="hidden" name="hidden"></div>
-			<div>number: <input type="number" name="number"></div>
-			<div>search: <input type="search" name="search"></div>
-			<div>datetime-local: <input type="datetime-local" name="datetime-local"></div>
+			<div>text: <input type="text" name="text" value="text"></div>
+			<div>password: <input type="password" name="password" value="password"></div>
+			<div>telephone: <input type="tel" name="telephone"></div>
+			<div>URL: <input type="url" name="url" value="url"></div>
+			<div>date: <input type="date" name="date" value="date"></div>
+			<div>time: <input type="time" name="time" value="time"></div>
+			<div>week: <input type="week" name="week" value="week"></div>
+			<div>color: <input type="color" name="color" value="color"></div>
+			<div>email: <input type="email" name="email" vaue="email"></div>
+			<div>month: <input type="month" name="month" value="month"></div>
+			<div>range: <input type="range" name="range" value="range"></div>
+			<div>hidden: <input type="hidden" name="hidden" value="hidden"></div>
+			<div>number: <input type="number" name="number" value="number"></div>
+			<div>search: <input type="search" name="search" value="search"></div>
+			<div>
+				datetime-local:
+				<input type="datetime-local" name="datetime-local" value="datetime-local">
+			</div>
 			<button>Submit</button>
 
 		</form>
