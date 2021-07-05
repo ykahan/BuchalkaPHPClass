f99af38b01e72b89c011ea6ef1637e01bb6a9fc5
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-07-05 12:58:09 +0300

    Created boilerplate file with php var_dump in the head

---
+++ Code/htmlBoilerplate.php
@@ -0,0 +1,18 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+	var_dump($_POST);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My Boilerplate</title>
+  <link rel="stylesheet" type="text/css" title="Cool stylesheet" href="style.css">
+</head>
+<body>
+  <div id="root"></div>
+  <script src="main.js" type="text/javascript"></script>
+</body>
+</html>
