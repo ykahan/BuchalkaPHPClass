f557dd2308828e59c8230255cdf54e3863463ad9
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-07-05 13:00:29 +0300

    Gave boilerplate file an appropriate form

--- Code/htmlBoilerplate.php
+++ Code/htmlBoilerplate.php
@@ -6,13 +6,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My Boilerplate</title>
-  <link rel="stylesheet" type="text/css" title="Cool stylesheet" href="style.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>My Boilerplate</title>
+	<link rel="stylesheet" type="text/css" title="Cool stylesheet" href="style.css">
 </head>
 <body>
-  <div id="root"></div>
-  <script src="main.js" type="text/javascript"></script>
+	<div id="root">
+		<form method="POST">
+		</form>
+	</div>
+	<script src="main.js" type="text/javascript"></script>
 </body>
 </html>
