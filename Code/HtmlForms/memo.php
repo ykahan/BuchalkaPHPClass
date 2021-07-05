d867cf9fb74d59251d4f3b3ab14ebd309c3afde7
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-07-05 12:54:05 +0300

    memo.php for studying textarea

---
+++ Code/HtmlForms/memo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Memo</title>
+  <link rel="stylesheet" type="text/css" title="Cool stylesheet" href="style.css">
+</head>
+<body>
+  <div id="root">
+	<form>
+		<div>Title: <input type="text" name="title" value="Title"></div>
+		<div>Body: <textarea name="body">Body</textarea></div>
+		<button>Submit</button>
+	</form></div>
+  <script src="main.js" type="text/javascript"></script>
+</body>
+</html>
